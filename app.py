9cfd53859fac3639df8baf0c51193ecc899af58e
Jisel Pandelo <jisel.pandelo@habber.com>
2025-10-07 10:49:39 +0200

    creating conflict to test

--- app.py
+++ app.py
@@ -2,4 +2,5 @@ print("hola git")
 'print("segunda l�nea")' 
 print("cambios por interfaz")
 print("from feature branch")
-print("second commit from feature branch") 
+print("second commit from feature branch")
+print ("create a change to create conflict")
